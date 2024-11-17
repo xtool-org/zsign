5e430c1fc7827dc69e8711de0dfa62f8fbaf8da3
Kabir Oberai <oberai.kabir@gmail.com>
2024-11-17 18:20:55 -0500

    ceremony

--- Sources/Zupersign/include/Zupersign.h
+++ Sources/Zupersign/include/Zupersign.h
@@ -1,5 +1,11 @@
 #include <signer.h>
 
+#pragma clang assume_nonnull begin
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int zsign_sign(const char *app_directory,
                const void *cert_data,
                size_t cert_len,
@@ -9,8 +15,14 @@ int zsign_sign(const char *app_directory,
                size_t num_entitlements,
                void (*progress)(const void *, double),
                const void *progressContext,
-               char **exception);
+               char * _Nullable * _Nonnull exception);
 
 void *zsign_analyze(const char *path,
                     size_t *out_len,
-                    char **exception);
+                    char * _Nullable * _Nonnull exception);
+
+#ifdef __cplusplus
+}
+#endif
+
+#pragma clang assume_nonnull end
